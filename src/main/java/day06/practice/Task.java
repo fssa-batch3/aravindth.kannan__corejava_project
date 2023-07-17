8ca6591938655b66325d83a869495a64fe30d321
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-07-17 23:23:02 +0530

    day 06 practice 2

---
+++ src/main/java/day06/practice/Task.java
@@ -0,0 +1,28 @@
+package day06.practice;
+import java.lang.reflect.Array;
+
+import java.util.ArrayList;
+
+public class Task {
+	public String taskName;
+    public int priority;
+    
+     public Task(String name, int p) {
+		this.taskName = name;
+		this.priority = p;
+	}
+	public String fullString() {
+		return "Task name" + taskName + "Priority" + priority;
+	}
+	
+
+	public static void main(String[] args) {
+		ArrayList<Task> arr = new ArrayList<>();
+		arr.add(new Task("Aravindth", 4));
+		arr.add(new Task("Kavi", 5));
+		System.out.println(arr);
+		
+		
+	}
+
+}
