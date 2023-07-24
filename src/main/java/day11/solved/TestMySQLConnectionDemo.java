3b7c0f32aa3cdb30aa7844f4cf87bf1ef18fa132
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-07-24 12:14:38 +0530

    day 11 jdbc connection solved

---
+++ src/main/java/day11/solved/TestMySQLConnectionDemo.java
@@ -0,0 +1,19 @@
+package day11.solved;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class TestMySQLConnectionDemo {
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "123456");
+		System.out.println(conn);
+
+	}
+}
