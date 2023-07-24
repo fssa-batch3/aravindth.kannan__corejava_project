a9bcc27ed0d76d7b538fded144f7272c56c70b22
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-07-24 12:33:08 +0530

    day 11 all solved complete

--- src/main/java/day11/solved/ConnectionUtil.java
+++ src/main/java/day11/solved/ConnectionUtil.java
@@ -8,40 +8,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-	 
-    public static Connection getConnection()  {
- 
-        Connection con = null;
-        String url = "jdbc:mysql://localhost/YOURDB";
-        String userName = "USERNAME";
-        String passWord = "PASSWORD";
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, userName, passWord);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to connect to the database");
-        }
-        return con;
-    }
-     
-    public static void close(Connection conn , Statement stmt, ResultSet rs){
-         
-        try
-        {
-            if ( rs != null ){
-                rs.close();
-            }
-            if ( stmt != null ) {
-                stmt.close();
-            }
-            if ( conn != null ){
-                conn.close();
-            }
-        }
-        catch(SQLException e){
-             e.printStackTrace();
-						 // No need re throw the exception.
-        }
-    }
+
+	public static Connection getConnection() throws SQLException {
+
+		Connection con = null;
+		String url = "jdbc:mysql://localhost:3306/project";
+		String userName = "root";
+		String passWord = "123456";
+
+		con = DriverManager.getConnection(url, userName, passWord);
+
+		return con;
+	}
+
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			// No need re throw the exception.
+		}
+	}
 }
